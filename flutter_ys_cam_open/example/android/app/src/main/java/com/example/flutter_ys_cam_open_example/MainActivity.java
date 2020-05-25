a8c1916177e8866001169e4ecd0cf02eccb2b615
leo <gitea@fake.local>
2020-05-25 13:46:20 +0800

    插件工程初始化创建

---
+++ flutter_ys_cam_open/example/android/app/src/main/java/com/example/flutter_ys_cam_open_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutter_ys_cam_open_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ flutter_ys_cam_open/ios/Classes/FlutterYsCamOpenPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterYsCamOpenPlugin.h"
+
+@implementation FlutterYsCamOpenPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_ys_cam_open"
+            binaryMessenger:[registrar messenger]];
+  FlutterYsCamOpenPlugin* instance = [[FlutterYsCamOpenPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
